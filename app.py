4e91a02bdc6b14066dad596d6242eec2bd82f6c0
omkilpady-ux <omkilpady@gmail.com>
2026-01-21 11:29:25 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Bond Dashboard", layout="wide")
+
+st.title("Composite Edge Bond Dashboard")
+
+st.write("If you can see this, Streamlit is working 🎉")
+st.write("Next step: plug in NSE bond data.")
